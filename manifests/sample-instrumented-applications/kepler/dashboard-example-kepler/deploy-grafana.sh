2741902f91eadfd871b9a6da881e5341beffd701
Sally O'Malley <somalley@redhat.com>
2023-07-13 14:33:28 -0400

    updates

--- manifests/sample-instrumented-applications/kepler/dashboard-example-kepler/deploy-grafana.sh
+++ manifests/sample-instrumented-applications/kepler/dashboard-example-kepler/deploy-grafana.sh
@@ -9,23 +9,23 @@ done
 oc apply -f $(pwd)/dashboard/02-grafana-instance.yaml
 oc apply -f $(pwd)/dashboard/03-grafana-sa-token-secret.yaml
 
-SERVICE_ACCOUNT=thanos-querier
+SERVICE_ACCOUNT=sample-monitoring-stack-prometheus
 SECRET=grafana-sa-token
 
-while ! oc get serviceaccount $SERVICE_ACCOUNT -n thanos
+while ! oc get serviceaccount $SERVICE_ACCOUNT -n observability
 do
     sleep 2
 done
 # Define Prometheus datasource
-oc adm policy add-cluster-role-to-user cluster-monitoring-view -z $SERVICE_ACCOUNT -n thanos
+oc adm policy add-cluster-role-to-user cluster-monitoring-view -z $SERVICE_ACCOUNT -n observability
 
 
-export BEARER_TOKEN=$(oc get secret ${SECRET} -o json -n thanos | jq -Mr '.data.token' | base64 -d) || or true
-# Get bearer token for `thanos-querier`
+export BEARER_TOKEN=$(oc get secret ${SECRET} -o json -n observability | jq -Mr '.data.token' | base64 -d) || or true
+# Get bearer token for `sample-monitoring-stack-prometheus`
 while [ -z "$BEARER_TOKEN" ]
 do
     echo waiting for service account token
-    export BEARER_TOKEN=$(oc get secret ${SECRET} -o json -n thanos | jq -Mr '.data.token' | base64 -d) || or true
+    export BEARER_TOKEN=$(oc get secret ${SECRET} -o json -n observability | jq -Mr '.data.token' | base64 -d) || or true
     sleep 1
 done
 echo service account token is populated, will now create grafana datasource
